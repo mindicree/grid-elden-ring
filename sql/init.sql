7def79bc6047da56153fab34b335c40a4b13bccb
Tyler <tsumararaiko@gmail.com>
2022-07-05 14:32:22 -0400

    corrected syntax in user table

--- sql/init.sql
+++ sql/init.sql
@@ -9,11 +9,11 @@ CREATE TABLE User (
     lname VARCHAR(255) NOT NULL,
     user_id VARCHAR(255) NOT NULL,
     pwd VARCHAR(255) NOT NULL,
-    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
     login_key VARCHAR(255),
     primary_role VARCHAR(255),
     secondary_role VARCHAR(255),
-    employment_status VARCHAR(255),
+    employment_status VARCHAR(255)
 );
 
 INSERT INTO User (fname, lname, user_id, pwd, is_admin) VALUES ('Billy', 'Mays', 'test_admin', '$2y$10$clvV7HA0bmgVBjnwZKYJ/OiGFBR.5R/yNSi8KFY7UVBHdhzR32Wru', TRUE);
