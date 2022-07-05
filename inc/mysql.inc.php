210f7f3f5b2033ddf04faaec723d990002c420c1
Tyler Johnson <tsumararaiko@gmail.com>
2022-07-04 21:00:16 -0400

    created file for connecting to db using env var

---
+++ inc/mysql.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+//values for connecting to mysql database
+$db_server = $_SERVER['DB_SERVER'];
+$db_username = $_SERVER['DB_USERNAME'];
+$db_password = $_SERVER['DB_PASSWORD'];
+$db_name = $_SERVER['DB_NAME'];
+
+//create connection to mysql database
+$db_connection = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+
+//if connection could not be established, kill process
+if (!$db_connection) { die ('Error connecting to database\nMessage: ' . mysqli_connect_error()); }
+
