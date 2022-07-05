75f4bc1ff83fbbb7336e02f636b508eb2d9605bf
Tyler Johnson <tsumararaiko@gmail.com>
2022-07-04 21:01:21 -0400

    removed systemlog table and added user table

--- sql/init.sql
+++ sql/init.sql
@@ -3,27 +3,18 @@ DROP DATABASE IF EXISTS GRID_DB;
 CREATE DATABASE IF NOT EXISTS GRID_DB;
 USE GRID_DB;
 
-CREATE TABLE SystemLog (
+CREATE TABLE User (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    old_coa VARCHAR(32),
-    new_coa VARCHAR(32),
-    serial_no VARCHAR(32) NOT NULL,
-    brand VARCHAR(32) NOT NULL,
-    model VARCHAR(32) NOT NULL,
-    os VARCHAR(32) NOT NULL,
-    computer_type VARCHAR(32) NOT NULL,
-    source VARCHAR(32) NOT NULL,
-    cpu_brand VARCHAR(32) NOT NULL,
-    cpu_model VARCHAR(32) NOT NULL,
-    cpu_gen VARCHAR(32) NOT NULL,
-    cpu_speed FLOAT(4, 2) NOT NULL,
-    ram VARCHAR(8) NOT NULL,
-    hdd VARCHAR(8) NOT NULL,
-    disk_drive VARCHAR(32) NOT NULL,
-    tags JSON,
-    tech VARCHAR(32) NOT NULL,
-    price FLOAT(10, 2),
-    dt_initial_system_log DATETIME,
-    dt_initial_irl_log DATETIME,
-    dt_last_update DATETIME
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255) NOT NULL,
+    pwd VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+    login_key VARCHAR(255),
+    primary_role VARCHAR(255),
+    secondary_role VARCHAR(255),
+    employment_status VARCHAR(255),
 );
+
+INSERT INTO User (fname, lname, user_id, pwd, is_admin) VALUES ('Billy', 'Mays', 'test_admin', '$2y$10$clvV7HA0bmgVBjnwZKYJ/OiGFBR.5R/yNSi8KFY7UVBHdhzR32Wru', TRUE);
+INSERT INTO User (fname, lname, user_id, pwd, is_admin) VALUES ('Dick', 'Tracy', 'test_user', '$2y$10$KFUzxGTLxJ2dRnrxSYCBs.wPgxxBlIqGgfkRihqamOnhU195026XG', FALSE);
