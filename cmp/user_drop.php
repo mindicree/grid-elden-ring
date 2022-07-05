7ee9543c007ba062cd170d4613dad50e4919b198
Tyler <tsumararaiko@gmail.com>
2022-07-05 19:07:35 -0400

    added dashboard page and tested restrict cmp

--- cmp/user_drop.php
+++ cmp/user_drop.php
@@ -3,7 +3,7 @@
         <?php echo $_SESSION["fname"]; ?>
     </button>
     <ul class="dropdown-menu dropdown-menu-end">
-        <li><a class="dropdown-item" href="#">Dashboard</a></li>
+        <li><a class="dropdown-item" href="dashboard.php">Dashboard</a></li>
         <li><a class="dropdown-item" href="#">Settings</a></li>
         <li><hr class="dropdown-divider"></li>
         <li><a class="dropdown-item" href="inc/logout.inc.php">Logout</a></li>
---
+++ dashboard.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once 'cmp/head.php'; ?>
+    <?php include_once 'cmp/restrict.php'; ?>
+    <title>Dashboard</title>
+</head>
+<body>
+    <?php include_once 'cmp/nav.php'; ?>
+</body>
+</html>
