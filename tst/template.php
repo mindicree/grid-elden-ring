f75c29c3302161d87ba40c9eef3a34e612d7632b
Tyler Johnson <tsumararaiko@gmail.com>
2022-07-05 22:21:34 -0400

    created template file for future pages

---
+++ tst/template.php
@@ -0,0 +1,18 @@
+<!-- 
+    THIS IS A TEMPLATE FOR PAGES. PLEASE ADVISE THE FOLLOWING
+    1. RENAME TITLE OF PAGE
+    2. UNCOMMENT RESTRICT IF NEEDED
+    3. MOVE FILE TO CORRECT DIRECTORY
+    4. REMOVE THIS COMMENT
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'].'/cmp/head.cmp.php'; ?> 
+    <?php //include_once $_SERVER['DOCUMENT_ROOT'].'/inc/restrict.inc.php'; ?>
+    <title>RENAME TITLE</title>
+</head>
+<body>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'].'/cmp/nav.cmp.php'; ?>
+</body>
+</html>
