4d64f9d4687fac075d5f683e2bf56f98c6e23653
Tyler Johnson <tsumararaiko@gmail.com>
2022-07-05 23:24:28 -0400

    added explicit route to cmp files to includes

--- cmp/nav.cmp.php
+++ cmp/nav.cmp.php
@@ -38,9 +38,9 @@
         </div>
         <?php 
             if (isset($_SESSION['user_key'])) {
-                include_once 'cmp/user_drop.cmp.php';
+                include_once $_SERVER['DOCUMENT_ROOT'].'/cmp/user_drop.cmp.php';
             } else {
-                include_once 'cmp/login_btn.cmp.php';
+                include_once $_SERVER['DOCUMENT_ROOT'].'/cmp/login_btn.cmp.php';
             }
         ?>
     </div>
