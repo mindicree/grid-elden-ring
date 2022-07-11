b05b78b951e66b68b4252abf430701b13bb3c339
Tyler Johnson <tsumararaiko@gmail.com>
2022-07-11 18:55:16 -0400

    added mysql include to template

--- tst/template.php
+++ tst/template.php
@@ -1,7 +1,7 @@
 <!-- 
     THIS IS A TEMPLATE FOR PAGES. PLEASE ADVISE THE FOLLOWING
     1. RENAME TITLE OF PAGE
-    2. UNCOMMENT RESTRICT/RESTRICT ADMIN IF NEEDED
+    2. UNCOMMENT RESTRICT/RESTRICT ADMIN/MYSQL IF NEEDED
     3. MOVE FILE TO CORRECT DIRECTORY
     4. REMOVE THIS COMMENT
 -->
@@ -11,6 +11,7 @@
     <?php include_once $_SERVER['DOCUMENT_ROOT'].'/cmp/head.cmp.php'; ?> 
     <?php //include_once $_SERVER['DOCUMENT_ROOT'].'/inc/restrict.inc.php'; ?>
     <?php //include_once $_SERVER['DOCUMENT_ROOT'].'/inc/restrict_admin.inc.php'; ?>
+    <?php //include_once $_SERVER['DOCUMENT_ROOT'].'/inc/mysql.inc.php'; ?>
     <title>RENAME TITLE</title>
 </head>
 <body>
