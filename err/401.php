d00d2cc8a8e6569d71cd3618a99d804fd0253d02
Tyler Johnson <tsumararaiko@gmail.com>
2022-07-10 20:18:16 -0400

    added 401 error page and corrected 404 error page

--- err/401.php
+++ err/401.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <?php include_once $_SERVER['DOCUMENT_ROOT'].'/cmp/head.cmp.php'; ?> 
-    <title>404 ERROR</title>
+    <title>401 ERROR</title>
 </head>
 <body>
     <?php include_once $_SERVER['DOCUMENT_ROOT'].'/cmp/nav.cmp.php'; ?>
     <div style='display: grid; grid-template-columns: 1fr; width: 100%; margin: auto; padding-top: 20vh; height: 30vh;'>
-        <h1 style='margin: auto;'>404 ERROR</h1>
-        <p style='margin: auto;'>Thank you mario, but our princess is in another castle</p>
+        <h1 style='margin: auto;'>401 ERROR</h1>
+        <p style='margin: auto;'>YOU! SHALL NOT! PASS!!!</p>
     </div>
 </body>
 </html>
