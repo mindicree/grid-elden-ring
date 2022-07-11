eedbc3af389b42b65cafa4c56e6f904b24489e6c
Tyler Johnson <tsumararaiko@gmail.com>
2022-07-10 20:19:57 -0400

    401 should atually be a 403

--- err/403.php
+++ err/403.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
     <?php include_once $_SERVER['DOCUMENT_ROOT'].'/cmp/head.cmp.php'; ?> 
-    <title>401 ERROR</title>
+    <title>403 ERROR</title>
 </head>
 <body>
     <?php include_once $_SERVER['DOCUMENT_ROOT'].'/cmp/nav.cmp.php'; ?>
     <div style='display: grid; grid-template-columns: 1fr; width: 100%; margin: auto; padding-top: 20vh; height: 30vh;'>
-        <h1 style='margin: auto;'>401 ERROR</h1>
+        <h1 style='margin: auto;'>403 ERROR</h1>
         <p style='margin: auto;'>YOU! SHALL NOT! PASS!!!</p>
     </div>
 </body>
