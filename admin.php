205354f19c919f5885679c41bd807b37a1b6a293
Tyler Johnson <tsumararaiko@gmail.com>
2022-07-10 20:09:57 -0400

    created an admin page

---
+++ admin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'].'/cmp/head.cmp.php'; ?> 
+    <?php //include_once $_SERVER['DOCUMENT_ROOT'].'/inc/restrict.inc.php'; ?>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'].'/inc/restrict_admin.inc.php'; ?>
+    <title>Admin Panel</title>
+</head>
+<body>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'].'/cmp/nav.cmp.php'; ?>
+</body>
+</html>
