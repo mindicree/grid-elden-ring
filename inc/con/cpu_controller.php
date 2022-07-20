0a563536e874a37141ba12b35a96b3111de5ea04
Tyler <tsumararaiko@gmail.com>
2022-07-19 21:11:35 -0400

    replaced unneeded prepared statements w.  query

--- inc/con/cpu_controller.php
+++ inc/con/cpu_controller.php
@@ -3,41 +3,13 @@
 function get_cpu_brand_list($db) {
     // prepared statement setup
     $sql_code = "SELECT brand AS cpu_brand FROM CPUModel GROUP BY brand";
-    $sql_statement = mysqli_stmt_init($db);
-
-    //check if statement is prepared, otherwise, return sql error
-    if (mysqli_stmt_prepare($sql_statement, $sql_code)) {
-        mysqli_stmt_bind_param($sql_statement);
-        mysqli_stmt_execute($sql_statement);
-        $result = mysqli_stmt_get_result($sql_statement);
-
-        if ($data = mysqli_fetch_all($result, MYSQLI_ASSOC)) {
-            return $data;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
-    }
+    $result = mysqli_query($db, $sql_code);
+    return $result;
 }
 
 function get_cpu_speed_list_active($db) {
     // prepared statement setup
     $sql_code = "SELECT name AS cpu_speed FROM CPUSpeed WHERE is_active = TRUE";
-    $sql_statement = mysqli_stmt_init($db);
-
-    //check if statement is prepared, otherwise, return sql error
-    if (mysqli_stmt_prepare($sql_statement, $sql_code)) {
-        mysqli_stmt_bind_param($sql_statement);
-        mysqli_stmt_execute($sql_statement);
-        $result = mysqli_stmt_get_result($sql_statement);
-
-        if ($data = mysqli_fetch_all($result, MYSQLI_ASSOC)) {
-            return $data;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
-    }
+    $result = mysqli_query($db, $sql_code);
+    return $result;
 }
